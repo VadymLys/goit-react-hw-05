24c0526d8dd79cefacbb45f9ffaa8246f9655045
Vadym <vadymlysytskyi@gmail.com>
2024-04-13 09:53:01 +0200

    4

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -17,8 +17,8 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/movies" element={<MoviesPage />} />
         <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
-          <Route path="Movies/:movieId/cast" element={<MovieCast />} />
-          <Route path="Movies/:movieId/reviews" element={<MovieReviews />} />
+          <Route path="movies/:movieId/cast" element={<MovieCast />} />
+          <Route path="movies/:movieId/reviews" element={<MovieReviews />} />
         </Route>
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
